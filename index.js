4f021beb7244fc4dd9416ddd3472940c80d6508e
code-recursion <helloajaysingh1@gmail.com>
2023-01-28 21:19:23 +0530

    rest api with crud

--- index.js
+++ index.js
@@ -1,136 +1,142 @@
-const express = require('express')
-const app = express()
-// const bodyParser = require("body-parser");
-app.use(express.json())
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+const Note = require("./mongo");
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-const cors = require('cors')
-
-app.use(cors())
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-    completed: false
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-    completed: true
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-    completed: false
-  }
-]
+const cors = require("cors");
+const { config } = require("./utils/config");
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('-----------------------')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("-----------------------");
+  next();
+};
 
-app.use(requestLogger)
+app.use(requestLogger);
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  console.log("mongod", config.MONGODB_URI);
 
-app.get('/api/notes', (request, response, next) => {
-  response.json(notes)
-})
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/api/notes/stats', (request, response, next) => {
-  const stats = {
-    status: "server is running",
-    count: notes.length,
-    date: new Date()
+app.get("/api/notes", async (request, response, next) => {
+  try {
+    const notes = await Note.find({});
+    response.json(notes);
+  } catch (error) {
+    response.json({
+      message: "error while fetching data",
+      error: error.message,
+    });
   }
+});
+
+app.get("/api/notes/stats", async (request, response, next) => {
+  const totalNotes = await Note.count({});
   response.send(`
-    <p>Server is up</p>
-    <p>${JSON.stringify(stats)}</p>
-  `)
-}
-)
+      <p>Server is up!  </p>
+      <p>last request made : ${new Date().toLocaleString()}</p>
+      <p>Total notes ${totalNotes}</p>
+    `);
+});
 
-app.get('/api/notes/:id', (request, response, next) => {
-  const id = Number(request.params.id)
-  const noteFound = notes.find(note => note.id === id)
-  if (noteFound) {
-    return response.json(noteFound)
-  } else {
-    return response.status(404).json({ message: "note not found" })
+app.get("/api/notes/:id", async (request, response, next) => {
+  try {
+    const id = request.params.id;
+    const noteFound = await Note.findById(id);
+    if (noteFound) {
+      response.json(noteFound);
+    } else {
+      response.json({ message: `note with given id ${id} not found` });
+    }
+  } catch (error) {
+    response.json({
+      message: "error while fetching data",
+      error: error.message,
+    });
   }
-})
+});
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body
-  if (!body.content) {
-    return response.status(400).json({ message: 'content is missing' })
+app.post("/api/notes", (request, response, next) => {
+  const { note } = request.body;
+  if (!note.content) {
+    return response.status(400).json({ message: "content is missing" });
   }
 
-  const note = {
-    id: generateId(),
-    content: body.content,
+  const newNote = new Note({
+    content: note.content,
     date: new Date(),
-    important: body.important || false,
-    completed: body.completed
+    isImportant: note.isImportant | false,
+    isCompleted: false,
+  });
+
+  newNote
+    .save()
+    .then((res) => {
+      console.log("res", res);
+      response.json(res);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      response.json(err);
+    });
+});
+
+app.put("/api/notes", async (request, response, next) => {
+  try {
+    const { note } = request.body
+    const noteFound = await Note.findById(note.id);
+    if (noteFound) {
+      const res = await Note.findByIdAndUpdate(note.id, note)
+      response.json({ message: "note updated", res })
+    } else {
+      response.json({ message: `note with given id ${id} not found` });
+    }
   }
-
-  notes = notes.concat(note)
-  console.log(note)
-  response.json(note)
-})
-
-app.put('/api/notes', (request, response, next) => {
-  const id = Number(request.params.id)
-  const existingNote = notes.find((note) => note.id === id)
-  const body = request.body
-  if (!body) {
-    return response.status(400).json({ message: 'missing data' })
+  catch (error) {
+    response.staus(500).json({ message: "error while fetching data", error: error.message })
   }
-  if (existingNote) {
-    console.log('exist', existingNote)
+});
+
+app.delete("/api/notes/:id", async (request, response, next) => {
+  const id = request.params.id
+  // notes = notes.filter((note) => note.id !== id);
+  try {
+    const noteFound = await Note.findById(id);
+    if (noteFound) {
+      await Note.findByIdAndRemove(id)
+      response.status(200).json({ success: true, message: `note with id ${id}, deleted successfully` });
+    } else {
+      response.json({ success: false, message: `note with given id ${id} not found` });
+    }
+  } catch (error) {
+    console.log('error occured while deleting', error);
+    response.status(500).json({ message: "error occured while deleting", error })
   }
-  const updatedNote = { ...existingNote, ...request.body, updatedDate: new Date() }
+});
 
-  notes = notes.map((note) => note.id === updatedNote.id ? updatedNote : note)
-  console.log('notes', notes)
-  response.status(200).send({ message: 'note updated successfully', updatedNote })
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-const PORT = 3001
+const PORT = config.PORT | 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port http://127.0.0.1:${PORT}`)
-})
+  console.log(`Server running on port http://127.0.0.1:${PORT}`);
+});
 
 const unknownEndpoint = (request, response, next) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
 }
 
-app.use(unknownEndpoint)
+app.use(errorHandler)
---
+++ utils/config.js
@@ -0,0 +1,8 @@
+const config = {
+  PORT: process.env.PORT,
+  MONGODB_URI: process.env.MONGODB_URI
+}
+
+module.exports = {
+  config
+}
